1b615db7cd7ff2b5c45b85ce8f7a547ee4077434
Charbonnier Robin <charbonnierrobin@gmail.com>
2020-07-02 23:20:35 +0200

    Update
    Home page : redesigned appearance
    Advice page : advice add

--- screens/AdviceScreen.js
+++ screens/AdviceScreen.js
@@ -43,6 +43,23 @@ function AdviceScreen({ navigation }) {
 function SportAdviceScreen({ navigation }) {
   return(
     <View style={{ flex: 1, alignItems: 'center' }}>
+      
+      <View style={{ margin:15, padding:5, borderColor: 'gray', borderWidth:1, alignItems: 'center' }}>
+        <Text style={ styles.title }>Hydratation</Text>
+        <Text style={{ textAlign:'center' }}>
+          Boire est le meilleur moyen de s'hydrater. Cette action doit être faite avant, pendant et après le sport.
+          Les accidents musculaire, les troubles digestifs et la réduction des performances sont des examples des conséquences que peut produire une déshydratation.
+          L'eau est la boisson la plus conseillée lorsqu'on pratique du sport cependant pour les efforts de plus de 1h les boissons protéiné peuvent apporter un vrai coup de pouce.
+        </Text>
+      </View>
+
+      <View style={{ margin:15, padding:5, borderColor: 'gray', borderWidth:1, alignItems: 'center' }}>
+        <Text style={ styles.title }>La récupération</Text>
+        <Text style={{ textAlign:'center' }}>
+          Les sportifs ont tendance à oublier de récupérer, pourtant le corps à besoin de retrouver son équilibre après des efforts.
+        </Text>
+      </View>
+
       <Button title="Retour aux conseils" color="#27B8AF" onPress={() => navigation.navigate('Advice')} />
       <Button title="Retour" color="#27B8AF" onPress={() => navigation.goBack()} />
     </View>
@@ -51,7 +68,27 @@ function SportAdviceScreen({ navigation }) {
 
 function NutritionAdviceScreen({ navigation }) {
   return(
-    <View style={{ flex: 1, alignItems: 'center' }}>
+    <View style={{ flex: 1, alignItems: 'center'  }}>
+      
+      <View style={{ margin:15, padding:5, borderColor: 'gray', borderWidth:1, alignItems: 'center' }}>
+        <Text style={ styles.title }>Les fruits et les légumes</Text>
+        <Text style={{ textAlign:'center' }}>
+          Ils sont de faibles apports caloriques mais ils couvrent les apports nutritionnels en vitamines et 
+          minéraux qui auront un impact direct sur la bonne assimilation des nutriments. 
+          Ils sont également riches en fibres et facilitent ainsi le transit intestinal.
+        </Text>
+      </View>
+      
+      <View style={{ margin:15, padding:5, borderColor: 'gray', borderWidth:1, alignItems: 'center' }}>
+        <Text style={ styles.title }>L'alcool</Text>
+        <Text style={{ textAlign:'center' }}>
+          L’alcool n’est pas recommandé chez les sportifs. 
+          Les calories apportées par ce dernier ne sont pas utilisables pour l’effort musculaire. 
+          L’alcool réduit les capacités physiques et va augmenter les toxines que l’organisme devra éliminer. 
+          De plus il accélère la déshydratation et modifie les capacités de régulation de la transpiration.
+        </Text>
+      </View>
+      
       <Button title="Retour aux conseils" color="#27B8AF" onPress={() => navigation.navigate('Advice')} />
       <Button title="Retour" color="#27B8AF" onPress={() => navigation.goBack()} />
     </View>
@@ -79,30 +116,17 @@ const styles = StyleSheet.create({
     height:200, 
     width:-100
   },
+  title:{
+    marginTop:5, 
+    marginHorizontal:5, 
+    fontWeight:"bold", 
+    fontSize:20
+  },
   container: {
     flex: 1,
     backgroundColor: '#fafafa',
   },
   contentContainer: {
     paddingTop: 15,
-  },
-  optionIconContainer: {
-    marginRight: 12,
-  },
-  option: {
-    backgroundColor: '#fdfdfd',
-    paddingHorizontal: 15,
-    paddingVertical: 15,
-    borderWidth: StyleSheet.hairlineWidth,
-    borderBottomWidth: 0,
-    borderColor: '#ededed',
-  },
-  lastOption: {
-    borderBottomWidth: StyleSheet.hairlineWidth,
-  },
-  optionText: {
-    fontSize: 15,
-    alignSelf: 'flex-start',
-    marginTop: 1,
   },
 });
